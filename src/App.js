2a7ec97ad0638a7501bd1a1676146a47747c4619
Arun3sh <m.aruneshwar@gmail.com>
2022-06-21 22:08:00 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import "./App.css";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
+import { useState, createContext } from "react";
 import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Navigationbar from "./components/Navbar/Navigationbar";
+import { Route, Switch, useLocation } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Createaccount from "./components/Customer/CreateAccount/Createaccount";
+
+export const authContext = createContext(null);
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const location = useLocation();
+  const modes = {
+    login: login,
+    setLogin: setLogin,
+    isLoading: isLoading,
+    setIsLoading: setIsLoading,
+    isAdmin: isAdmin,
+    setIsAdmin: setIsAdmin,
+  };
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <authContext.Provider value={modes}>
+      <div className="App">
+        {location.pathname !== "/login" &&
+          location.pathname !== "/create-account" && <Navigationbar />}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/create-account">
+            <Createaccount />
+          </Route>
+        </Switch>
+        {location.pathname !== "/login" &&
+          location.pathname !== "/create-account" && <Footer />}
+      </div>
+    </authContext.Provider>
   );
 }
 
